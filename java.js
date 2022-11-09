80bc76fd84ff84abb24d77c36e31e28051c91c1b
marmoregranito <106874223+cadastrode@users.noreply.github.com>
2022-11-09 07:14:02 -0300

    substituição

---
+++ java.js
@@ -0,0 +1,28 @@
+
+let mover=1;
+
+document.getElementById("slide1").checked = true;
+
+function kMenu(){
+   
+if( men.style.display == "block"){
+   men.style.display = "none";
+}else{
+    men.style.display= "block";
+}
+}
+setInterval(function(){
+    auto()
+},10000)
+function auto(){
+    mover++;
+    if(mover>5){
+        mover=1;
+    }
+
+    document.getElementById("slide"+mover).checked = true;
+
+}
+
+
+
